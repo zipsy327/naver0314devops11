0247501208327fa2190740454870860ed03047c1
leemoonhee <zipsy327@gmail.com>
2024-03-21 12:28:37 +0900

    0321수업

--- javawork/Naver0314JavaProject/src/day0319/Ex8_String.java
+++ javawork/Naver0314JavaProject/src/day0319/Ex8_String.java
@@ -43,6 +43,7 @@ public class Ex8_String {
 		System.out.println("apple".compareTo("append"));//'l','e' : 7(양수:첫문자열이 더 크다),첫문자열이 작을경우 음수가 나온다
 		System.out.println("apple".compareTo("banana"));//'a','b' 비교 : -1
 		System.out.println("apple".compareTo("apple"));//완전히 같을경우 0
+		System.out.println("computer".compareTo("apple"));//첫문자열이 더 클경우 2
 		
 		//반복
 		System.out.println(str2.repeat(3));
