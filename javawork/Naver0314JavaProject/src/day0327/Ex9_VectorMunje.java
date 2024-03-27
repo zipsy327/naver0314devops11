ed0c2d43c12e67f9d26241dd20d19c877c9334ae
leemoonhee <zipsy327@gmail.com>
2024-03-27 17:52:27 +0900

    0327수업

---
+++ javawork/Naver0314JavaProject/src/day0327/Ex9_VectorMunje.java
@@ -0,0 +1,20 @@
+package day0327;
+
+/*
+ *   1.학생정보추가 : 이름,나이,3과목 점수입력
+ *   2.학생정보삭제 : 이름으로 찾아서 삭제(없을경우 메세지 출력)
+ *   3.전체출력 : 번호,이름,Java,Spring,HTML,총점,평균,등급 출력
+ *   4.평균으로검색 :  평균값을 입력하면 그 평균값 이상의 학생들을 출력
+ *   5.이름으로검색 : 입력한이름을 포함하고 있으면 출력(contains)
+ *   6.저장후종료
+ */
+public class Ex9_VectorMunje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("컴퓨터".contains("컴"));//true
+		//System.out.println("컴퓨터".contains("하"));//false
+		
+	}
+
+}
