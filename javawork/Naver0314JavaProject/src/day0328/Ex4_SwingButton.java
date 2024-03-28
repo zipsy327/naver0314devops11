7604ece65d5ee55a4db94d69109a02c3f636ed83
leemoonhee <zipsy327@gmail.com>
2024-03-28 14:54:49 +0900

    0328수업

--- javawork/Naver0314JavaProject/src/day0328/Ex4_SwingButton.java
+++ javawork/Naver0314JavaProject/src/day0328/Ex4_SwingButton.java
@@ -10,6 +10,8 @@ import javax.swing.JOptionPane;
 
 public class Ex4_SwingButton extends JFrame{
 	JButton btn1,btn2;
+	JButton []btnArray=new JButton[6];//각 배열의 값 null
+	String []btnLabel= {"초록색","빨강색","분홍색","오렌지색","노랑색","랜덤색"};
 	
 	public Ex4_SwingButton(String title) {
 		// TODO Auto-generated constructor stub
@@ -47,6 +49,64 @@ public class Ex4_SwingButton extends JFrame{
 		btn2.setBounds(130, 10, 100, 30);//x,y,w,h
 		this.add(btn2);
 		btn2.addActionListener(new Button2Event());
+		
+		//btnArray 배열 변수를 생성후 프레임에 추가
+		int yPos=50;
+		for(int i=0;i<btnArray.length;i++)
+		{
+			btnArray[i]=new JButton(btnLabel[i]);
+			btnArray[i].setBounds(10, yPos, 100, 30);
+			this.add(btnArray[i]);
+			yPos+=35;
+			
+			//익명 내부 클래스 형태로 이벤트 구현
+			btnArray[i].addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					//Object ob=e.getSource();//이벤트가 발생한 오브젝트
+					JButton ob=(JButton)e.getSource();
+					System.out.println(ob.getText());//초록색		
+					//System.out.println(ob.getLabel());//초록색
+					
+					switch(ob.getText())
+					{
+					case "빨강색":
+						//부모컴포넌트의 인스턴스 변수인 this 를 통해서 배경색 변경
+						Ex4_SwingButton.this.getContentPane().setBackground(Color.red);
+						break;
+					case "초록색":
+						//부모컴포넌트의 인스턴스 변수인 this 를 통해서 배경색 변경
+						Ex4_SwingButton.this.getContentPane().setBackground(Color.green);
+						break;
+					case "분홍색":
+						//부모컴포넌트의 인스턴스 변수인 this 를 통해서 배경색 변경
+						Ex4_SwingButton.this.getContentPane().setBackground(Color.magenta);
+						break;
+					case "오렌지색":
+						//부모컴포넌트의 인스턴스 변수인 this 를 통해서 배경색 변경
+						Ex4_SwingButton.this.getContentPane().setBackground(Color.orange);
+						break;
+					case "노랑색":
+						//부모컴포넌트의 인스턴스 변수인 this 를 통해서 배경색 변경
+						Ex4_SwingButton.this.getContentPane().setBackground(Color.yellow);
+						break;
+					case "랜덤색":
+						//0~255 의 임의의 숫자 r,g,b 를 구한후 new Color 생성자를 통해서 변경
+					{
+						int r=(int)(Math.random()*256);
+						int g=(int)(Math.random()*256);
+						int b=(int)(Math.random()*256);
+						Ex4_SwingButton.this.getContentPane().setBackground(new Color(r, g, b));
+						
+					}
+						break;
+					}
+				}
+			});
+		}
+		
 	}
 	
 	//버튼 2 이벤트
